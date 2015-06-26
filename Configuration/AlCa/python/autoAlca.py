d1cb73c5d6d1c590d58c1814d5201f6bc8b41ef3
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2015-06-26 20:20:50 +0200

    New version of the AlCa matrix.
    The Alca matrix has a new structure in order to take into account the data processing scenarios, which differ between Run1 and Run2, as well as the PD definition, which is bound to HLT menu changing from cosmics to collision.
    Each data processing scenario (same name as in Config.DP) is the key of the matrix: its value is a dictionary, containing the mapping between the PD and the AlCa producers.
    There is still the possibility to define the list of AlCa producers to be run in Express and Prompt Reconstruction.

--- Configuration/AlCa/python/autoAlca.py
+++ Configuration/AlCa/python/autoAlca.py
@@ -1,26 +1,54 @@
-AlCaRecoMatrix = {'ExpressCosmics' : 'SiStripCalZeroBias+TkAlCosmics0T',
-                  'StreamExpress'  : 'SiStripCalZeroBias+TkAlMinBias+MuAlCalIsolatedMu+DtCalib',
-                  'MinimumBias'    : 'SiStripCalMinBias+TkAlMinBias',
-                  'Commissioning'  : 'HcalCalIsoTrk',
-                  'SingleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib',
-                  'DoubleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
-                  'MuOnia'         : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
-                  'SingleElectron' : 'EcalCalWElectron+EcalUncalWElectron',
-                  'DoubleElectron' : 'EcalCalZElectron+EcalUncalZElectron',
-                  'AlCaLumiPixels' : 'LumiPixels',
-                  'DoubleMuParked' : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
-                  'MuOniaParked'   : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
-                  'Cosmics'        : 'TkAlCosmics0T+MuAlGlobalCosmics+HcalCalHOCosmics+DtCalibCosmics',
-                  # These two cannot run on RAW, they are just meant to run on the dedicated AlcaRAW so they do not enter the allForPrompt list
-                  'AlCaP0'         : '',
-                  # ---------------------------------------------------------------------------------------------------------------------------
-                  'HcalNZS'        : 'HcalCalMinBias'
-                  # This is in the AlCaRecoMatrix, but no RelVals are produced
-                  # 'TestEnablesTracker' : 'TkAlLAS'
-                  }
+AlCaRecoMatrix = { 'cosmics'     : { 'AlCaLumiPixels'      : 'LumiPixels',
+                                     'Cosmics'             : 'TkAlCosmics0T+HcalCalHOCosmics+DtCalibCosmics+MuAlGlobalCosmics',
+                                     'ExpressCosmics'      : 'SiStripPCLHistos+SiStripCalZeroBias+TkAlCosmics0T+DtCalibCosmics',
+                                     'HcalHPDNoise'        : '',
+                                     'HcalNZS'             : 'HcalCalMinBias+HcalCalIterativePhiSym',
+                                     'MinimumBias'         : 'SiStripCalZeroBias+TkAlMinBias',
+                                     'NoBPTX'              : '',
+                                     },
+                   'pp'          : { 'AlCaLumiPixels'      : 'LumiPixels',
+                                     'Commissioning'       : 'HcalCalIsoTrk',
+                                     'DoubleElectron'      : 'EcalCalZElectron+EcalUncalZElectron',
+                                     'DoubleMu'            : 'TkAlZMuMu+DtCalib+MuAlCalIsolatedMu+MuAlOverlaps',
+                                     'DoubleMuParked'      : 'TkAlZMuMu+DtCalib+MuAlCalIsolatedMu+MuAlOverlaps',
+                                     'HcalNZS'             : 'HcalCalMinBias',
+                                     'MinimumBias'         : 'SiStripCalMinBias+TkAlMinBias',
+                                     'MuOnia'              : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
+                                     'MuOniaParked'        : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
+                                     'SingleElectron'      : 'EcalCalWElectron+EcalUncalWElectron',
+                                     'SingleMu'            : 'TkAlMuonIsolated+DtCalib+MuAlCalIsolatedMu+MuAlOverlaps',
+                                     'StreamExpress'       : 'SiStripPCLHistos+SiStripCalMinBias+SiStripCalZeroBias+TkAlMinBias+DtCalib+MuAlCalIsolatedMu',
+                                     'TestEnablesTracker'  : 'TkAlLAS'
+                                     },
+                   'cosmicsRun2' : { 'AlCaLumiPixels'      : 'LumiPixels',
+                                     'Cosmics'             : 'TkAlCosmics0T+HcalCalHOCosmics+DtCalibCosmics+MuAlGlobalCosmics',
+                                     'ExpressCosmics'      : 'SiStripPCLHistos+SiStripCalZeroBias+TkAlCosmics0T+DtCalibCosmics',
+                                     'HcalHPDNoise'        : '',
+                                     'HcalNZS'             : 'HcalCalMinBias+HcalCalIterativePhiSym',
+                                     'MinimumBias'         : 'SiStripCalZeroBias+TkAlMinBias',
+                                     'NoBPTX'              : '',
+                                     },
+                   'ppRun2'      : { 'AlCaLumiPixels'      : 'LumiPixels',
+                                     'Commissioning'       : 'HcalCalIsoTrk',
+                                     'Charmonium'          : 'TkAlJpsiMuMu',
+                                     'DoubleEG'            : 'EcalCalZElectron+EcalUncalZElectron+HcalCalIterativePhiSym',
+                                     'DoubleMu'            : 'TkAlZMuMu+MuAlCalIsolatedMu+MuAlOverlaps+MuAlZMuMu',
+                                     'HcalNZS'             : 'HcalCalMinBias',
+                                     'HLTPhysics'          : 'SiStripCalMinBias+TkAlMinBias',
+                                     'JetHT'               : 'HcalCalDijets',
+                                     'MET'                 : 'HcalCalNoise',
+                                     'MuOnia'              : 'TkAlUpsilonMuMu',
+                                     'SingleElectron'      : 'EcalCalWElectron+EcalUncalWElectron+EcalCalZElectron+EcalUncalZElectron+HcalCalIterativePhiSym',
+                                     'SingleMu'            : 'TkAlMuonIsolated+HcalCalIterativePhiSym+DtCalib+MuAlCalIsolatedMu+MuAlOverlaps+MuAlZMuMu',
+                                     'SinglePhoton'        : 'HcalCalGammaJet',
+                                     'StreamExpress'       : 'SiStripPCLHistos+SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias+DtCalib',
+                                     'TestEnablesEcalHcal' : 'HcalCalPedestals',
+                                     'ZeroBias'            : 'SiStripCalZeroBias+TkAlMinBias+LumiPixelsMinBias',
+                                     },
+                 }
 
 def buildList(pdList, matrix):
-    """Takes a list of primary datasets (PDs) and the AlCaRecoMatrix (a dictinary) and returns a string with all the AlCaRecos for the selected PDs separated by the '+' character without duplicates."""
+    """Takes a list of primary datasets (PDs) and the AlCaRecoMatrix (a dictionary) and returns a string with all the AlCaRecos for the selected PDs separated by the '+' character without duplicates."""
     alCaRecoList = []
     for pd in pdList:
         alCaRecoList.extend(matrix[pd].split("+"))
@@ -35,8 +63,13 @@ def buildList(pdList, matrix):
     return stringList
 
 # Update the lists anytime a new PD is added to the matrix
-autoAlca = { 'allForPrompt'         : buildList(['MinimumBias', 'Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'DoubleMuParked', 'MuOniaParked', 'SingleElectron', 'DoubleElectron', 'HcalNZS'], AlCaRecoMatrix),
-             'allForExpress'        : buildList(['StreamExpress'], AlCaRecoMatrix),
-             'allForPromptCosmics'  : buildList(['Cosmics'], AlCaRecoMatrix),
-             'allForExpressCosmics' : buildList(['ExpressCosmics'], AlCaRecoMatrix) }
+autoAlca = { 'allForExpress'            : buildList( [ 'StreamExpress' ], AlCaRecoMatrix[ 'pp' ] ),
+             'allForExpressCosmics'     : buildList( [ 'ExpressCosmics' ], AlCaRecoMatrix[ 'cosmics' ] ),
+             'allForPrompt'             : buildList( [ 'MinimumBias', 'Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'DoubleMuParked', 'MuOniaParked', 'SingleElectron', 'DoubleElectron', 'HcalNZS' ], AlCaRecoMatrix[ 'pp' ] ),
+             'allForPromptCosmics'      : buildList( [ 'Cosmics' ], AlCaRecoMatrix[ 'cosmics' ] ),
+             'allForExpressRun2'        : buildList( [ 'StreamExpress' ], AlCaRecoMatrix[ 'ppRun2' ] ),
+             'allForExpressCosmicsRun2' : buildList( [ 'ExpressCosmics' ], AlCaRecoMatrix[ 'cosmicsRun2' ] ),
+             'allForPromptRun2'         : buildList( [ 'Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'Charmonium', 'MET', 'JetHT', 'SingleElectron', 'SinglePhoton', 'DoubleEG', 'ZeroBias' ,'HLTPhysics', 'HcalNZS' ], AlCaRecoMatrix[ 'ppRun2' ] ),
+             'allForPromptCosmicsRun2'  : buildList( [ 'Cosmics' ], AlCaRecoMatrix[ 'cosmicsRun2' ] ),
+             }
 autoAlca.update(AlCaRecoMatrix)
