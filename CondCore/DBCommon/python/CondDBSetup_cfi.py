a3ab22c275f740056633ee9f2f238092ce886243
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-01-28 18:24:02 +0100

    Backward compatibility fix: make CondCore/DBCommon/python/CondDBCommon_cfi.py and CondCore/DBCommon/python/CondDBSetup_cfi.py deprecated and point to CondCore/CondDB/python/CondDB_cfi.py.

--- CondCore/DBCommon/python/CondDBSetup_cfi.py
+++ CondCore/DBCommon/python/CondDBSetup_cfi.py
@@ -3,20 +3,7 @@ print " # WARNING: this module is deprecated.                             # "
 print " # Please use CondCore.CondDB.CondDB_cfi.py                        # "
 print " ################################################################### "
 
-import FWCore.ParameterSet.Config as cms
-
-CondDBSetup = cms.PSet(
-    DBParameters = cms.PSet(
-        authenticationPath = cms.untracked.string(''),
-        authenticationSystem = cms.untracked.int32(0),
-        connectionRetrialPeriod = cms.untracked.int32(10),
-        idleConnectionCleanupPeriod = cms.untracked.int32(10),
-        messageLevel = cms.untracked.int32(0),
-        enablePoolAutomaticCleanUp = cms.untracked.bool(False),
-        enableConnectionSharing = cms.untracked.bool(True),
-        connectionRetrialTimeOut = cms.untracked.int32(60),
-        connectionTimeOut = cms.untracked.int32(60),
-        enableReadOnlySessionOnUpdateConnection = cms.untracked.bool(False)
-    )
-)
+from CondCore.CondDB.CondDB_cfi import *
+CondDBSetup = CondDB.clone()
+CondDBSetup.__delattr__('connect')
 
